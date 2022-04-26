90a62a06b953b9f10f7d58f304f579d2798aaba6
Stavros G <75627879+stavrify@users.noreply.github.com>
2022-04-26 22:32:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+
+function pageTransition() {
+    
+    var tl = gsap.timeline();
+
+    tl.to('ul.transition li', {duration: .4, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
+    tl.to('ul.transition li', {duration: .4, scaleY: 0, transformOrigin: "bottom left", delay: .1})
+}
+
+function contentAnimation() {
+    
+    var tl = gsap.timeline();
+    tl.from('h1', { duration: 1.4, translateY: -500, opacity: 0})
+    tl.to('.center', { transform:"rotateX(0deg)"}, "-=1")
+    
+}
+
+function afterdiv() {
+
+    var tl = gsap.timeline();
+    tl.from('.navy', {duration: 1.5, translateY: 300,opacity: 0, stagger: .2})
+    tl.to('.navy', {translateY: 0,opacity: 1, stagger: .3})
+
+}
+
+function delay(n) {
+    n = n || 2000;
+    return new Promise(done => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+
+barba.init({
+
+    sync: true,
+
+    transitions: [{
+
+        async leave(data) {
+
+            const done = this.async();
+
+            pageTransition();
+            await delay(1150);
+            done();
+        },
+
+        async enter(data) {
+            contentAnimation();
+        },
+
+        async afterEnter(data){
+            afterdiv();
+        },
+
+        async once(data) {
+            contentAnimation();
+        },
+
+        async afterOnce(data) {
+            afterdiv();
+        }
+    }]
+})
