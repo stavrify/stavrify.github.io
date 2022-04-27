22ed5c445faf04d93a30d16a4643738f33814696
Stavros <stavrosga@hotmail.gr>
2022-04-27 16:27:18 +0300

    Added treasure game

---
+++ treasure-game/scripts/game.js
@@ -0,0 +1,208 @@
+
+  $(".opt1").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm2').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm1 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+    
+  });
+
+  $(".opt6").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm3').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm2 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+    
+  });
+  $(".opt8").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm4').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm3 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+    
+  });
+
+  $(".opt11").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm5').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm4 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+    
+  });
+
+  $(".opt15").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm6').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm5 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+    
+  });
+
+  $(".opt18").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm7').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm6 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+    
+  });
+
+
+  $(".opt19").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm8').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm7 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+    
+  });
+
+  $(".opt21").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm9').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm8 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $(".opt23").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm10').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm9 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+    
+  });
+
+  $(".opt25").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm12').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm10 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+    $("#colsm11").html("<img src=styles/keytaken.svg id=kleidi class=icon />");
+    $("#colsm11").removeClass("kleidicontainer")
+  });
+
+  $(".opt28").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm13').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm12 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $(".opt30").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm14').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm13 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $(".opt33").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm15').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm14 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $(".opt37").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm16').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm15 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $(".opt40").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm17').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm16 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $(".opt41").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm18').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm17 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+    bg = null;
+  });
+
+  $(".opt44").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm19').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm18 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $(".opt46").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm20').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm19 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $(".opt50").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm21').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm20 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $(".opt51").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm22').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm21 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $(".opt56").click(function() {
+    $('.player').remove();
+    $('.dropdown-menu').removeClass('show');
+    $('.player').css("display","none");
+    $('#colsm23').append('<img data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="styles/a.svg" class="player"/>');
+    $('.player').show(5000);
+    $('#colsm22 > h3').html("<img class=correctico src='styles/good.svg' /> ");
+  });
+
+  $('button.treasureunlock').click(function() {
+    $('body').css("background", "url(../styles/confetti.gif)");
+    $(".container").css("border-radius", "1em");
+    $(".container").css("background", "#e8ffff");
+    $(".container").html("<h1 class=bravo>Συγχαρητήρια!&nbsp;  Άνοιξες Με Επιτυχία Tον Θησαυρό! <br> <img src=./styles/treasureopened.svg class=treasureopened> </h1> <marquee>Δημιουργήθηκε από: Σταύρος Γκάτσης , Κάποια γραφικά έχουν ως πηγή τα: <a href=Freepik.com>Freepik</a> , <a href=Flaticon.com >Flaticon.com</a> . ©2019 </marquee>");
+    new Audio(Url + treasuresfx).play();
+});
---
+++ treasure-game/scripts/sounds.js
@@ -0,0 +1,36 @@
+var bg;
+var Url = "../assets/sounds/";
+var treasuresfx = "treasure.wav";
+var bg = "bg.mp3";
+var sound = "sfx.mp3";
+var badsfx = "sfxbad.wav";
+var bg = "bg.mp3";
+var keysfx = "key.wav";
+
+$(document).one("click" ,function() {
+    new Audio(Url + bg).play();
+});
+
+$('button.correct').click(function() {
+    new Audio(Url + sound).play();
+    $('body').prepend('<div class="alert fade show alert-success" role="alert">Μπράβο! Το βρήκες!</div>')
+    setTimeout(function() {
+        $(".alert-success").alert('close')
+    }, 2500);
+});
+
+$('button.incorrect').click(function() {
+    new Audio(Url + badsfx).play();
+    $('body').prepend('<div class="alert fade show alert-warning" role="alert">Σκέψου το καλύτερα και ξαναπροσπάθησε...</div>')
+    setTimeout(function() {
+        $(".alert-warning").alert('close')
+    }, 2500);
+});
+
+$('button.correctkey').click(function() {
+    new Audio(Url + keysfx).play();
+    $('body').prepend('<div class="alert keyalert fade show " role="alert">Συγχαρητήρια! Πήρες το κλειδί! <br> <img src="styles/key.svg" id="kleidi" class="icon" /> </div>')
+    setTimeout(function() {
+        $(".keyalert").alert('close')
+    }, 5000);
+});
